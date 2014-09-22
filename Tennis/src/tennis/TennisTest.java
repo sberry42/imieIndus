5bd623bfec496fa80e0cb3917930b5ae72dbca62
Loïc Désirest <ldesirest@gmail.com>
2014-09-22 15:13:33 +0200

    Ajout du deuxieme joueur dans les tests

--- Tennis/src/tennis/TennisTest.java
+++ Tennis/src/tennis/TennisTest.java
@@ -27,9 +27,11 @@ public class TennisTest
     {
         Jeu jeu = new Jeu();
         Joueur joueur1 = new Joueur("Joueur 1");
+        Joueur joueur2 = new Joueur("Joueur 2");
         Assert.assertEquals("0-0",jeu.score());
         jeu.setService(joueur1);
         Assert.assertEquals("Joueur 1", jeu.getService());
-        
+        jeu.setOtherPlayer(joueur2);
+        Assert.assertEquals("Joueur 2", jeu.getOtherPlayer());
     }
 }
